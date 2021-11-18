2e4a94e69a5dd1598f197d6e63e8fcc363839766
Simo Elalj <simo@elalj.com>
2021-11-18 11:42:08 +0100

    updated global design

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ export default {
   async fetch () {
     this.loading = true
     try {
-      const response = await this.$axios.$get('/lastDate')
+      const response = await this.$axios.$get('/api')
       this.lastDate = response.lastDate
     } catch (error) {
       this.errorMessage = error.message
